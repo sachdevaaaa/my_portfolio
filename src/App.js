69ededbb68f6e4b3f0700f07ee9859b63d6d439f
Divyam <divyamsachdeva777@gmail.com>
2024-09-21 02:58:28 +0530

    Fix ESLint warnings

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Services from './components/Services';
 import Skills from './components/Skills';
-import ProgressBar from './components/SkateboardCharacter';
+
 import SkateboardCharacter from './components/SkateboardCharacter';
-import TestimonialsSection from './components/TestimonialsSection';
+
 
 function App() {
   return (
